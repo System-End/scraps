e35bee306dffb3156ba0723f8d4f04beef8dd7c7
Nathan <70660308+NotARoomba@users.noreply.github.com>
2026-01-26 15:14:25 -0500

    Update +page.svelte

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -11,28 +11,28 @@
 		<p class="mb-2"><strong>ys:</strong> any project <Superscript number={1} tooltip="silly, nonsensical, or fun" /></p>
 
 		<p class="mb-6">
-			<strong>ws:</strong> random stuff from hq<Superscript number={2} tooltip="(including stickers)" /> (the more hours, the more stuff)
+			<strong>ws:</strong> random items from hq<Superscript number={2} tooltip="(including stickers)" /> (more hours, more stuff)
 		</p>
 
 		<p class="mb-6">
-			it's cold and wintry here at hack club hq<Superscript number={2} tooltip="in vermont" />. after prototype, overglade, milky way, and other
+			it's cold and wintery here<Superscript number={3} tooltip="in vermont" /> at hack club hq. after prototype, overglade, milkyway, and other
 			hackathons, there are boxes and boxes of items, "scraps," if you will. now, dear hack
-			clubber, this ysws is your chance to win the cool leftovers, including random hardware<Superscript number={3} tooltip="sensors, esp32s, arduinos, breadboards, and a singular resistor" />,
-			postcards, fudge, and maybe a secret surprise<Superscript number={4} tooltip="super cool" />!
+			clubber, this ysws is your chance to win the cool leftovers, including random hardware<Superscript number={4} tooltip="sensors, esp32s, arduinos, breadboards, and a singular resistor" />,
+			postcards, fudge, and maybe a secret surprise<Superscript number={5} tooltip="limited edition stickers" />!
 		</p>
 
 		<p class="text-xl font-bold mb-4">but how, you may ask?</p>
 
 		<p class="mb-6">
-			well, it's simple: you just ship any projects that are extra silly, nonsensical, or fun<Superscript number={5} tooltip="or literally anything" />, and
-			you will get a chance to spin a wheel for every 30 minutes of work you do!
+			well, it's simple: you just ship any projects that are extra silly, nonsensical, or fun<Superscript number={6} tooltip="or literally any project" />, and
+			you will get a chance to spin a wheel<Superscript number={7} tooltip="totally not gambling" /> for every 30 minutes of work you do!
 		</p>
 
 		<p class="text-xl font-bold mb-4">what's on the wheel?</p>
 
 		<p class="mb-6">
 			currently, there is a random assortment of hardware left over from prototype, postcards, the
-			famous vermont fudge<Superscript number={6} tooltip="fudge fudge fudge" />, and more items planned as events wrap up. oh, and the best part,
+			famous vermont fudge<Superscript number={8} tooltip="fudge fudge fudge" />, and more items planned as events wrap up. oh, and the best part,
 			<strong>stickers!</strong>
 		</p>
 
@@ -46,7 +46,7 @@
 			>
 				stickers.hackclub.com</a
 			>? well, here is your chance to get any sticker (that we have in stock) to complete your
-			collection<Superscript number={7} tooltip="soon to be made collection.hackclub.com to keep track of your sticker collection" />! this includes some of the rarest and most sought-after stickers from hack club.
+			collection<Superscript number={9} tooltip="soon to be made collection.hackclub.com to keep track of your sticker collection" />! this includes some of the rarest and most sought-after stickers from hack club.
 		</p>
 	</div>
 </div>
